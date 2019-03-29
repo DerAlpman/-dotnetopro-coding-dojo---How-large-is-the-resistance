a9d96752a816ae0ae4be97c42984c92866a549ff
Stefan Alpers <stefan.alpers@gmx.net>
2019-03-29 20:56:03 +0100

    Interface and class for color codes

---
+++ HowLargeIsTheResistance/Components.HowLargeIsTheResistance/Interfaces/IColorCode.cs
@@ -0,0 +1,11 @@
+﻿namespace Components.HowLargeIsTheResistance.Models
+{
+    public interface IColorCode
+    {
+        string Color { get; }
+        string Decimal { get; }
+        string Multiplier { get; }
+        string Tolerance { get; }
+        string UnitPosition { get; }
+    }
+}
---
+++ HowLargeIsTheResistance/HowLargeIsTheResistance/Models/ColorCode.cs
@@ -0,0 +1,27 @@
+﻿using System.Xml.Serialization;
+using Components.HowLargeIsTheResistance.Models;
+
+namespace HowLargeIsTheResistance.Models
+{
+    internal class ColorCode : IColorCode
+    {
+        #region PROPERTIES
+
+        [XmlAttribute]
+        public string Color { get; set; }
+
+        [XmlAttribute]
+        public string Decimal { get; set; }
+
+        [XmlAttribute]
+        public string UnitPosition { get; set; }
+
+        [XmlAttribute]
+        public string Multiplier { get; set; }
+
+        [XmlAttribute]
+        public string Tolerance { get; set; }
+
+        #endregion
+    }
+}
