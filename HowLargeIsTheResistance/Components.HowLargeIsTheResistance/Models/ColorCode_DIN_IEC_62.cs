11853f8ae135f340c546a3a53a3b57a773c120b3
Stefan Alpers <stefan.alpers@gmx.net>
2019-03-31 23:20:32 +0200

    removed everything concerning xml

--- HowLargeIsTheResistance/Components.HowLargeIsTheResistance/Models/ColorCode_DIN_IEC_62.cs
+++ HowLargeIsTheResistance/Components.HowLargeIsTheResistance/Models/ColorCode_DIN_IEC_62.cs
@@ -1,28 +1,20 @@
-﻿using System.Xml.Serialization;
-
-namespace Components.HowLargeIsTheResistance.Models
+﻿namespace Components.HowLargeIsTheResistance.Models
 {
     /// <summary>
     /// <para> A color code according to DIN IEC 62</para>
     /// </summary>
-    [XmlRoot("ColorCode_DIN_IEC_62")]
     public class ColorCode_DIN_IEC_62
     {
         #region PROPERTIES
 
-        [XmlAttribute]
         public string Color { get; set; }
 
-        [XmlAttribute]
         public string Decimal { get; set; }
 
-        [XmlAttribute]
         public string UnitPosition { get; set; }
 
-        [XmlAttribute]
         public string Multiplier { get; set; }
 
-        [XmlAttribute]
         public string Tolerance { get; set; }
 
         #endregion
